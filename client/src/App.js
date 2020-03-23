7bf8f6b80231683541344fca750b4f1112e48dd1
Michael Bidnyk <mbidnyk1@gmail.com>
2020-03-23 18:57:18 -0500

    movie cards and home button take you to correct url

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-
+import { Route, NavLink, Switch, useRouteMatch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
-
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
@@ -12,7 +13,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path='/movies/:movieID'>
+          <Movie />
+        </Route>
+        <Route path='/'>
+          <MovieList />
+        </Route>
+      </Switch>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useRouteMatch, Route, NavLink } from "react-router-dom";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+   const {movieID} = useParams();
+  console.log(movieID);
   useEffect(() => {
-    const id = 1;
+    const id = movieID;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
